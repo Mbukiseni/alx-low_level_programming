62d4bdeeeb17901e13ccc24af96ea0d55d245cc9
Mbukiseni <mbndima@gmail.com>
2023-06-22 18:49:56 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - checks for lowercase character
  * @c: The character to be tested
- * Return: 1 fro uppercase character or 0 for anything else
+ * Return: 1 for uppercase character or 0 for anything else
  */
 
 int _isupper(int x)

2ad5b9fa182be315259d36f2166e8443aa544faa
Mbukiseni <mbndima@gmail.com>
2023-06-26 10:07:04 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - In reverse the string is printed
+ * @s: String to print
+ * Return: 0
+ */
+
+void print_rev(char *s)
+
+{
+
+	int x = 0;
+	while (s[x] != '\0')
+	{
+	x++;
+	}
+	for (x -= 1; x >= 0; c--)
+	{
+	_putchar(s[x]);
+	}
+	_putchar('\n');
+
+}

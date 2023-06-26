3641c26f08cf4c8ad3d4ce1dac3312befe8a8558
Mbukiseni <mbndima@gmail.com>
2023-06-26 10:47:28 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *   _strcpy - Copy the string
+ *   @dest: The character to find
+ *   @src: Source value to check
+ *   Return: 0 (success)
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int x;
+for (x = 0; src[x] != '\0'; x++)
+{
+dest[i] = src[x];
+}
+dest[x++] = '\0';
+return (dest);
+}

f51bc2b3272dfa872419a8844229c69fe4498fae
Mbukiseni <mbndima@gmail.com>
2023-07-03 08:08:14 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - Set the value of a pointer
+ * @s: pointer
+ * @to: char
+ * Return: Always 0 (Success)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

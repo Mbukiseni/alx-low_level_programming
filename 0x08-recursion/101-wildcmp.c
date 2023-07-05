b601319b7c18a09ef032784cfb3ae180ffe1894d
Mbukiseni <mbndima@gmail.com>
2023-07-05 11:13:34 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -4,7 +4,7 @@
  * wildcmp - compares two strings and returns 1 if identical
  * @s1: string to be verified
  * @s2: string 2
- * Return: 1 or 0 
+ * Return: 1 or 0
  */
 int wildcmp(char *s1, char *s2)
 {

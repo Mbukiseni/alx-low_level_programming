fedb10bbc74af889538e6bb19262c1fcc7aeee0b
Mbukiseni <mbndima@gmail.com>
2023-07-17 06:33:06 -0700

    dog.h

--- 0x0D-preprocessor/dog.h
+++ 0x0D-preprocessor/dog.h
@@ -1,18 +1,25 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
- * struct dog - Dog structure definition
- * @name: Name of the dog
- * @age: The dog's age
- * @owner: Dog owner
- */
-typedef struct dog
+  * struct dog - Dog structure definition
+  * @name: Name of the dog
+  * @age: The dog's age
+  * @owner: Dog owner
+  *
+  * Description: Characteristics of the dog
+  */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+/**
+ * dog_t - Typedef for dog structure
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);

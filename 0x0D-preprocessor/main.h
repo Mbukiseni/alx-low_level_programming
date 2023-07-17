2b21abaa0d219ab8e4f77c65aa75a3c0001d60e0
Mbukiseni <mbndima@gmail.com>
2023-07-17 06:47:21 -0700

    main.h

---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#include <stdio.h>
+#include <stdlib.h>
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif

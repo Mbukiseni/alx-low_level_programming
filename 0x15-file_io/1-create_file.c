d46fc08dde314bd0dc69cf9a4f514f926026fb84
Mbukiseni <mbndima@gmail.com>
2023-08-07 06:25:33 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,6 @@
  *
  * Return: string length
  */
-
 size_t _strlen(char *str)
 {
 	size_t i;
@@ -21,13 +20,12 @@ size_t _strlen(char *str)
 }
 
 /**
- * create_file - make a file
- * @filename: file name to make
- * @text_content: void terminated string to write to the file
+ * create_file - creates a file
+ * @filename: file name to create
+ * @text_content: NULL terminated string to write
  *
- * Return: 1 upon success, -1 upon failure
+ * Return: 1 or -1
  */
-
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
